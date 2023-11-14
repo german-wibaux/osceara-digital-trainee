a83373e19bb640de46fb7e7885411a5f9cdd5460
martin-dw <martin.coronel@digitalwest.com.ar>
2023-11-14 15:15:28 -0300

    Revert "Fix sidebar"
    This reverts commit 50fa48bf110577d1fe39deab814bac17fb58af80.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Title } from '@angular/platform-browser';
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
-  title = 'OSCEARA CoreUI Free Angular Admin Template';
+  title = 'CoreUI Free Angular Admin Template';
 
   constructor(
     private router: Router,
