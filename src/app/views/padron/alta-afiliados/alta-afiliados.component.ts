80198305559e33f8f4b3472ba648164e8b7df00f
yerman <germanwibaux@gmail.com>
2023-11-23 12:50:39 -0300

    Afiliados table

--- src/app/views/padron/alta-afiliados/alta-afiliados.component.ts
+++ src/app/views/padron/alta-afiliados/alta-afiliados.component.ts
@@ -6,6 +6,20 @@ interface formFiltro{
   ejecutado: string | null;
 }
 
+interface BackendIncome {
+  id: number;
+  activo: boolean;
+  cuil: number;
+  cuilTitular: number;
+  nroAfiliado: number | null;
+  nombre: string;
+  gerenciador: string;
+  nacimiento: Date;
+  edad: number;
+  provincia: string;
+  localidad: string;
+}
+
 @Component({
   selector: 'app-alta-afiliados',
   templateUrl: './alta-afiliados.component.html',
@@ -31,5 +45,55 @@ export class AltaAfiliadosComponent {
     console.log("Se ha enviado correctamente");
     console.log(filtroIncome);
   }
+
+  backendIncome: BackendIncome[] = [{
+    id: 1,
+    activo: true,
+    cuil: 11445002332,
+    cuilTitular: 11332201232,
+    nroAfiliado: 11332201232,
+    nombre: "Jill Valentine",
+    gerenciador: "B26-OSCEARA Cordoba/Sin",
+    nacimiento: new Date('02/21/2002'),
+    edad: 20,
+    provincia: "Cordoba",
+    localidad: "CORDOBA"
+  },{
+    id: 1,
+    activo: false,
+    cuil: 11445002332,
+    cuilTitular: 11332201232,
+    nroAfiliado: 11332201232,
+    nombre: "Jill Valentine",
+    gerenciador: "B26-OSCEARA Cordoba/Sin",
+    nacimiento: new Date('02/21/2002'),
+    edad: 20,
+    provincia: "Cordoba",
+    localidad: "CORDOBA"
+  },{
+    id: 1,
+    activo: true,
+    cuil: 11445002332,
+    cuilTitular: 11332201232,
+    nroAfiliado: 11332201232,
+    nombre: "Jill Valentine",
+    gerenciador: "B26-OSCEARA Cordoba/Sin",
+    nacimiento: new Date('02/21/2002'),
+    edad: 20,
+    provincia: "Cordoba",
+    localidad: "CORDOBA"
+  },{
+    id: 1,
+    activo: false,
+    cuil: 11445002332,
+    cuilTitular: 11332201232,
+    nroAfiliado: 11332201232,
+    nombre: "Jill Valentine",
+    gerenciador: "B26-OSCEARA Cordoba/Sin",
+    nacimiento: new Date('02/21/2002'),
+    edad: 20,
+    provincia: "Cordoba",
+    localidad: "CORDOBA"
+  },]
 
 }
